e9c69bacd56a73b27847a746767059403d710a17
pedrocalazans05 <pedrocalazans05@gmail.com>
2023-08-14 15:13:03 -0300

    v1.03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export function App() {
+
+  const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function handleUsuario(evento){
+    setUsuario(evento.target.value)
+  }
+
+  function handleSenha(evento){
+    setSenha(evento.target.value)
+  }
 
   return(
     <>
@@ -13,11 +24,21 @@ export function App() {
       <form action="">
         <div>
           <label htmlFor="usuario">Usuário</label>
-          <input type="text" id="usuario" placeholder='Digite seu usuário'/>
+          <input 
+            type="text" 
+            id="usuario" 
+            placeholder='Digite seu usuário'
+            onChange={handleUsuario}
+            />
         </div>
         <div>
         <label htmlFor="senha">Senha</label>
-          <input type="text" id="senha" placeholder='Digite sua senha'/>
+          <input 
+            type="text" 
+            id="senha" 
+            placeholder='Digite sua senha'
+            onChange={handleSenha}
+            />
         </div>
         <button>
           <p>Entrar</p>
